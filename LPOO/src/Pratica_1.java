979d88ebb180196dbc17e84f1b4b40b2a106d4a6
nuno1 <nuno1@172.30.12.103>
2017-02-16 16:03:58 +0000

    Scanner incomplete

--- LPOO/src/Pratica_1.java
+++ LPOO/src/Pratica_1.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class Pratica_1 {
 	public static void main(String[] args)
@@ -13,12 +14,22 @@ public class Pratica_1 {
 							{'X',' ','I',' ','I',' ','X','K',' ','X'},
 							{'X','X','X','X','X','X','X','X','X','X'}};
 		
+	
 		for(int i=0; i<10;i++){
 			for(int j=0; j<10;j++){
 				System.out.print(matrix[i][j]);
 				System.out.print(' ');
 			}
 			System.out.print('\n');
+		}
+		
+		System.out.println("Introduza um carater (w,s,a,d) para mover o heroi"); 
+		Scanner input = new Scanner(System.in);
+		switch(input.next()){
+		case "w":
+				if()
+			
+			
 		}
 	}	
 }
