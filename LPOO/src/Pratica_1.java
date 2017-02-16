5bb8bba5e2036247090ce42f9bde2843648b1351
WORK PC <WORK PC@172.30.0.39>
2017-02-16 16:08:12 +0000

    New Stuff

--- LPOO/src/Pratica_1.java
+++ LPOO/src/Pratica_1.java
@@ -1,25 +1,37 @@
-
+import java.util.*;
 public class Pratica_1 {
-	public static void main(String[] args)
+	public static int hero_x = 1, hero_y = 1;
+	public static char[][] matrix = {{'X','X','X','X','X','X','X','X','X','X'},
+									{'X','H',' ',' ','I',' ','X',' ','G','X'},
+									{'X','X','X',' ','X','X','X',' ',' ','X'},
+									{'X',' ','I',' ','I',' ','X',' ',' ','X'},
+									{'X','X','X',' ','X','X','X',' ',' ','X'},
+									{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+									{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+									{'X','X','X',' ','X','X','X','X',' ','X'},
+									{'X',' ','I',' ','I',' ','X','K',' ','X'},
+									{'X','X','X','X','X','X','X','X','X','X'}};
+	public static void display()
 	{
-		char[][] matrix = {{'X','X','X','X','X','X','X','X','X','X'},
-							{'X','H',' ',' ','I',' ','X',' ','G','X'},
-							{'X','X','X',' ','X','X','X',' ',' ','X'},
-							{'X',' ','I',' ','I',' ','X',' ',' ','X'},
-							{'X','X','X',' ','X','X','X',' ',' ','X'},
-							{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
-							{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
-							{'X','X','X',' ','X','X','X','X',' ','X'},
-							{'X',' ','I',' ','I',' ','X','K',' ','X'},
-							{'X','X','X','X','X','X','X','X','X','X'}};
-		
-		for(int i=0; i<10;i++){
-			for(int j=0; j<10;j++){
-				System.out.print(matrix[i][j]);
-				System.out.print(' ');
-			}
+		for (int i = 0; i < 50; ++i)
+			System.out.println("");
+		for (int i = 0; i < 10; ++i)
+		{
+			for (int j = 0; j < 10; ++j)
+				System.out.print(matrix[i][j] + " ");
 			System.out.print('\n');
 		}
+	}
+	public static void update()
+	{
+		
+	}
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		//switch(input.next())
+		
+		
 	}	
 }
 
