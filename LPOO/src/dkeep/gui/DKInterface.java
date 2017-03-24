94a51da13f1691cdf777a9ef5ad10e002130d9dd
WORK PC <WORK PC@LAPTOP-PC.home>
2017-03-24 00:22:20 +0000

    Iteration 5, task 3 done

--- LPOO/src/dkeep/gui/DKInterface.java
+++ LPOO/src/dkeep/gui/DKInterface.java
@@ -13,19 +13,21 @@ import java.awt.Font;
 import javax.swing.JComboBox;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
-import java.awt.event.ItemListener;
-import java.awt.event.ItemEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class DKInterface
 {
 	private ByteArrayOutputStream stos;
 	private PrintStream stream;
 	
-	private DungeonKeep.State game_state = DungeonKeep.State.LEVEL_PLAYING; // Acts as null
+	private DungeonKeep.State game_state = null; // Acts as null
 	private int guard_type = 0;
 	private int ogre_number = 0;
 	private int current_guard_type = 0;
@@ -44,7 +46,7 @@ public class DKInterface
 	private JButton bttnDirectionLeft = new JButton("Left");
 	private JButton bttnDirectionRight = new JButton("Right");
 	private JButton bttnDirectionDown = new JButton("Down");
-
+	
 	/**
 	 * Launch the application.
 	 */
@@ -54,6 +56,7 @@ public class DKInterface
 				try {
 					DKInterface window = new DKInterface();
 					window.frmDungeonKeep.setVisible(true);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -110,10 +113,8 @@ public class DKInterface
 				bttnDirectionRight.setEnabled(false);
 				bttnDirectionDown.setEnabled(false);
 				break;
-			default:
-				lbl1GameStatus.setText("");
-				lbl2GameStatus.setText("");
 		}
+		txtrMap.requestFocusInWindow();
 	}
 
 	/**
@@ -126,35 +127,73 @@ public class DKInterface
 		frmDungeonKeep.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmDungeonKeep.getContentPane().setLayout(null);
 		
+		txtrMap.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent mos_evn) {
+				lbl1GameStatus.setText("");
+				lbl2GameStatus.setText("");
+				txtrMap.requestFocusInWindow();
+			}
+		});
+		txtrMap.addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent key_evn) {
+				if (game_state == DungeonKeep.State.LEVEL_PLAYING)
+				{
+					lbl1GameStatus.setText("");
+					lbl2GameStatus.setText("");
+					switch (key_evn.getKeyCode())
+					{
+					case KeyEvent.VK_UP:
+						game_state = level.update("w");
+						break;
+					case KeyEvent.VK_LEFT:
+						game_state = level.update("a");
+						break;
+					case KeyEvent.VK_RIGHT:
+						game_state = level.update("d");
+						break;
+					case KeyEvent.VK_DOWN:
+						game_state = level.update("s");
+						break;
+					default:
+						return;
+					}
+					update();
+				}
+			}
+
+			@Override
+			public void keyTyped(KeyEvent key_evn) {
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent key_evn) {
+			}
+		});
 		txtrMap.setTabSize(4);
 		txtrMap.setEditable(false);
 		txtrMap.setFont(new Font("Consolas", Font.PLAIN, 12));
-		txtrMap.setBounds(300, 70, 320, 240);
+		txtrMap.setBounds(300, 80, 300, 200);
 		frmDungeonKeep.getContentPane().add(txtrMap);
 		
+		lbl1GameStatus.setFocusable(false);
 		lbl1GameStatus.setFont(new Font("Consolas", Font.PLAIN, 12));
-		lbl1GameStatus.setBounds(300, 25, 320, 20);
+		lbl1GameStatus.setBounds(300, 25, 300, 20);
 		frmDungeonKeep.getContentPane().add(lbl1GameStatus);
 		
+		lbl2GameStatus.setFocusable(false);
 		lbl2GameStatus.setFont(new Font("Consolas", Font.PLAIN, 12));
 		lbl2GameStatus.setBounds(300, 45, 320, 20);
 		frmDungeonKeep.getContentPane().add(lbl2GameStatus);
 		
-		// ==
-		
-		JButton bttnNewGame = new JButton("New Game");
-		JButton bttnExitGame = new JButton("Exit Game");
-		
-		JTextField txtfldNumberOfOgres = new JTextField();
-		JComboBox cmbbxGuardPersonality = new JComboBox(new String[]{"Rookie", "Drunken", "Suspicious"});
-		
-		// ==
-		
 		JLabel lblNumberOfOgres = new JLabel("Number of Ogres:");
+		lblNumberOfOgres.setFocusable(false);
 		lblNumberOfOgres.setFont(new Font("Consolas", Font.PLAIN, 12));
 		lblNumberOfOgres.setBounds(20, 20, 120, 20);
 		frmDungeonKeep.getContentPane().add(lblNumberOfOgres);
-		
+
+		JTextField txtfldNumberOfOgres = new JTextField();
 		txtfldNumberOfOgres.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusGained(FocusEvent foc_evn) {
@@ -166,7 +205,7 @@ public class DKInterface
 				if (!txtfldNumberOfOgres.getText().equals("")) {
 					try {
 						ogre_number = Integer.parseInt(txtfldNumberOfOgres.getText());
-						if (ogre_number > 5 || ogre_number < 1) {
+						if (ogre_number < 1 || ogre_number > 5) {
 							ogre_number = 0;
 							throw new NumberFormatException();
 						}
@@ -177,15 +216,18 @@ public class DKInterface
 						lbl2GameStatus.setText("Must be a number between 1 and 5.");
 					}
 				}
+				else {
+					ogre_number = 0;
+				}
 				if (guard_type * ogre_number == 0) {
 					lbl1GameStatus.setText("Please select game settings");
 					lbl2GameStatus.setText("");
 				}
 				else {
-					lbl1GameStatus.setText("You can now start a new game");
+					lbl1GameStatus.setText("");
 					lbl2GameStatus.setText("");
-				}
-			}			
+				}	
+			}
 		});
 		txtfldNumberOfOgres.setColumns(5);
 		txtfldNumberOfOgres.setFont(new Font("Consolas", Font.PLAIN, 12));
@@ -193,10 +235,12 @@ public class DKInterface
 		frmDungeonKeep.getContentPane().add(txtfldNumberOfOgres);
 		
 		JLabel lblGuardPersonality = new JLabel("Guard personality:");
+		lblGuardPersonality.setFocusable(false);
 		lblGuardPersonality.setFont(new Font("Consolas", Font.PLAIN, 12));
 		lblGuardPersonality.setBounds(20, 50, 140, 20);
 		frmDungeonKeep.getContentPane().add(lblGuardPersonality);
 		
+		JComboBox cmbbxGuardPersonality = new JComboBox(new String[]{"Rookie", "Drunken", "Suspicious"});
 		cmbbxGuardPersonality.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusGained(FocusEvent foc_evn) {
@@ -211,7 +255,7 @@ public class DKInterface
 					lbl2GameStatus.setText("");
 				}
 				else {
-					lbl1GameStatus.setText("You can now start a new game");
+					lbl1GameStatus.setText("");
 					lbl2GameStatus.setText("");
 				}
 			}
@@ -221,9 +265,12 @@ public class DKInterface
 		cmbbxGuardPersonality.setBounds(160, 50, 100, 20);
 		frmDungeonKeep.getContentPane().add(cmbbxGuardPersonality);
 		
+		JButton bttnNewGame = new JButton("New Game");
 		bttnNewGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent act_evn) {
 				if (guard_type * ogre_number != 0) {
+					lbl1GameStatus.setText("You can now play");
+					lbl2GameStatus.setText("");
 					game_state = DungeonKeep.State.LEVEL_PLAYING;
 					level = new DungeonKeep(level_number = 0, current_guard_type = guard_type, current_ogre_number = ogre_number);
 					bttnRestart.setEnabled(true);
@@ -237,7 +284,6 @@ public class DKInterface
 					lbl1GameStatus.setText("Invalid settings!");
 					lbl2GameStatus.setText("Please select game settings");
 				}
-				
 			}
 		});
 		bttnNewGame.setFont(new Font("Consolas", Font.PLAIN, 12));
@@ -246,6 +292,8 @@ public class DKInterface
 		
 		bttnRestart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent act_evn) {
+				lbl1GameStatus.setText("You can now play");
+				lbl2GameStatus.setText("");
 				game_state = DungeonKeep.State.LEVEL_PLAYING;
 				level = new DungeonKeep(level_number, current_guard_type, current_ogre_number);
 				bttnDirectionUp.setEnabled(true);
@@ -255,6 +303,7 @@ public class DKInterface
 				update();
 			}
 		});
+		bttnRestart.setFocusable(false);
 		bttnRestart.setEnabled(false);
 		bttnRestart.setFont(new Font("Consolas", Font.PLAIN, 12));
 		bttnRestart.setBounds(150, 110, 100, 20);
@@ -262,6 +311,8 @@ public class DKInterface
 		
 		bttnNext.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent act_evn) {
+				lbl1GameStatus.setText("You can now play");
+				lbl2GameStatus.setText("");
 				game_state = DungeonKeep.State.LEVEL_PLAYING;
 				level = new DungeonKeep(++level_number, current_guard_type, current_ogre_number);
 				bttnNext.setEnabled(false);
@@ -275,6 +326,7 @@ public class DKInterface
 				update();
 			}
 		});
+		bttnNext.setFocusable(false);
 		bttnNext.setEnabled(false);
 		bttnNext.setVisible(false);
 		bttnNext.setFont(new Font("Consolas", Font.PLAIN, 12));
@@ -283,10 +335,13 @@ public class DKInterface
 		
 		bttnDirectionUp.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent act_evn) {
+				lbl1GameStatus.setText("");
+				lbl2GameStatus.setText("");
 				game_state = level.update("w");
 				update();
 			}
 		});
+		bttnDirectionUp.setFocusable(false);
 		bttnDirectionUp.setEnabled(false);
 		bttnDirectionUp.setFont(new Font("Consolas", Font.PLAIN, 12));
 		bttnDirectionUp.setBounds(100, 170, 80, 20);
@@ -294,10 +349,13 @@ public class DKInterface
 		
 		bttnDirectionLeft.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent act_evn) {
+				lbl1GameStatus.setText("");
+				lbl2GameStatus.setText("");
 				game_state = level.update("a");
 				update();
 			}
 		});
+		bttnDirectionLeft.setFocusable(false);
 		bttnDirectionLeft.setEnabled(false);
 		bttnDirectionLeft.setFont(new Font("Consolas", Font.PLAIN, 12));
 		bttnDirectionLeft.setBounds(50, 200, 80, 20);
@@ -305,10 +363,13 @@ public class DKInterface
 		
 		bttnDirectionRight.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent act_evn) {
+				lbl1GameStatus.setText("");
+				lbl2GameStatus.setText("");
 				game_state = level.update("d");
 				update();
 			}
 		});
+		bttnDirectionRight.setFocusable(false);
 		bttnDirectionRight.setEnabled(false);
 		bttnDirectionRight.setFont(new Font("Consolas", Font.PLAIN, 12));
 		bttnDirectionRight.setBounds(150, 200, 80, 20);
@@ -316,20 +377,25 @@ public class DKInterface
 		
 		bttnDirectionDown.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent act_evn) {
+				lbl1GameStatus.setText("");
+				lbl2GameStatus.setText("");
 				game_state = level.update("s");
 				update();
 			}
 		});
+		bttnDirectionDown.setFocusable(false);
 		bttnDirectionDown.setEnabled(false);
 		bttnDirectionDown.setFont(new Font("Consolas", Font.PLAIN, 12));
 		bttnDirectionDown.setBounds(100, 230, 80, 20);
 		frmDungeonKeep.getContentPane().add(bttnDirectionDown);
 		
+		JButton bttnExitGame = new JButton("Exit Game");
 		bttnExitGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent act_evn) {
 				System.exit(0);
 			}
 		});
+		bttnExitGame.setFocusable(false);
 		bttnExitGame.setFont(new Font("Consolas", Font.PLAIN, 12));
 		bttnExitGame.setBounds(90, 290, 100, 20);
 		frmDungeonKeep.getContentPane().add(bttnExitGame);
