b719f026b2af55c76c3ff603b726737beb8ead52
ShinuziKyura <up201503716@fe.up.pt>
2017-06-11 21:59:22 +0100

    Add files via upload

--- CastleVsCastle/desktop/src/com/cvc/game/desktop/DesktopLauncher.java
+++ CastleVsCastle/desktop/src/com/cvc/game/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "CastleWar";
-		config.width = 640;
-		config.height = 360;
+		config.width = 1920;
+		config.height = 1080;
 		new LwjglApplication(new CVCGame(), config);
 	}
 }
